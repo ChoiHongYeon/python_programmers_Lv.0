c4423379f58352a9401774407c55192cf45f4ee2
ChoiHongYeon <cgo0168@naver.com>
2024-03-27 21:50:55 +0900

    first commit

---
+++ 20240327-1.py
@@ -0,0 +1,9 @@
+# flag에 따라 다른 값 반환하기(https://school.programmers.co.kr/learn/courses/30/lessons/181933)
+
+def solution(a, b, flag):
+    answer=0
+    if flag:
+        answer=a+b
+    else:
+        answer=a-b
+    return answer
