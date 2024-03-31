6634b2a8162a6ae2574759c142279e51d9086436
ChoiHongYeon <cgo0168@naver.com>
2024-03-31 14:27:28 +0900

    second commit

--- 20240331-2.py
+++ 20240331-2.py
@@ -2,11 +2,14 @@
 
 def solution(l, r):
     answer = []
+    k=[]
     for i in range(l,r+1):
-        if i%5==0:
+        for j in range(0,len(str(i))):
+            if str(i)[j]=="0" or str(i)[j]=="5":
+                k.append("True")
+            else:
+                k.append("False")
+        if "False" not in k:
             answer.append(i)
+        k=[]
     return [-1] if answer==[] else answer
-
-A=solution(5,555)
-B=solution(10,20)
-print(A,B)
