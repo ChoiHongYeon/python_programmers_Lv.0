71a51ec100c673bc11829c363a4d90e59629554c
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2025-02-07 19:43:30 +0900

    first commit

---
+++ 20250207-1.py
@@ -0,0 +1,8 @@
+# 중복된 숫자 개수 (https://school.programmers.co.kr/learn/courses/30/lessons/120583?language=python3)
+
+def solution(array, n):
+
+    return array.count(n)
+
+print(solution([1, 1, 2, 3, 4, 5], 1))
+print(solution([0, 2, 3, 4], 1))
